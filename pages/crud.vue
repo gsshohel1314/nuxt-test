78ee6456ba8c1a90420f9dce5b7ea7b12ffd618b
gsshohel1314 <gsshohel1314@gmail.com>
2022-11-16 18:13:26 +0600

    create modal open

--- pages/crud.vue
+++ pages/crud.vue
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
           <div class="card col-md-6">
             <div class="card-body">
-              <h5 class="card-title">Add New Product</h5>
+              <!-- <h5 class="card-title">Add New Product</h5>
               <form v-on:submit.prevent="onSubmit()" method="POST">
                 <div class="row">
                   <div class="form-group col-md-4">
@@ -20,10 +20,19 @@
                     <button type="submit" class="btn btn-primary">Add</button>
                   </div>
                 </div>
-              </form>
+              </form> -->
 
               <div class="mt-4">
-                <h5 class="card-title">Product List</h5>
+                <div class="row mb-2">
+                  <div class="col-md-8">
+                    <h5 class="card-title">Product List</h5>
+                  </div>
+                  <div class="col-md-4">
+                    <button type="button" class="btn btn-sm btn-primary float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">Add New</button>
+                  </div>
+                  <div class="clearfix"></div>
+                </div>
+
                 <table class="table table-bordered">
                   <thead>
                     <tr>
@@ -46,6 +55,26 @@
                   </tbody>
                 </table>
               </div>
+
+              <!-- start create modal -->
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- end create modal -->
 
             </div>
           </div>
